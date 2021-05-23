a47464c77992ddec6d28813b418cd6f9d5138a90
Xinyiqi <shinichi_watanabe@dignicate.com>
2021-05-24 00:06:19 +0900

    wip, creating the adapter and view holder.

--- app/src/main/java/com/dignicate/zero1/ui/main/MainFragment.kt
+++ app/src/main/java/com/dignicate/zero1/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.dignicate.zero1.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
@@ -32,4 +33,22 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    class Adapter(private val data: String,
+                  private val viewModel: MainViewModel
+    ) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    }
 }
