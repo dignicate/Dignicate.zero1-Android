20c1901f3c1767e78f17454d8bf64faf0dfc30dd
Xinyiqi <shinichi_watanabe@dignicate.com>
2021-05-23 22:39:15 +0900

    MainFragment

--- app/src/main/java/com/dignicate/zero1/OLDMainActivity.kt
+++ app/src/main/java/com/dignicate/zero1/OLDMainActivity.kt
@@ -2,15 +2,15 @@ package com.dignicate.zero1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dignicate.zero1.databinding.ActivityMainBinding
+//import com.dignicate.zero1.databinding.ActivityMainBinding
 
 class OLDMainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
+//    private lateinit var binding: ActivityMainBinding
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//    }
 }
--- app/src/main/java/com/dignicate/zero1/ui/main/MainFragment.kt
+++ app/src/main/java/com/dignicate/zero1/ui/main/MainFragment.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dignicate.zero1.R
+import com.dignicate.zero1.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,13 +14,16 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding: MainFragmentBinding
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
