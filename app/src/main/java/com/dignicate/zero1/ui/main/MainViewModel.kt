9169a9401a1d5cf2a9c0fd4c3a6f2d041caadcdc
Xinyiqi <shinichi_watanabe@dignicate.com>
2021-05-24 14:05:19 +0900

    wip, ContentStructure

--- app/src/main/java/com/dignicate/zero1/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dignicate/zero1/ui/main/MainViewModel.kt
@@ -4,24 +4,38 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    data class IndexPath(
-        val section: Int,
-        val row: Int
-    )
+    sealed class RowState {
+        class SectionRow(section: Section): RowState()
+        class ItemRow(item: Item): RowState()
+    }
 
     object ContentStructure {
 
-        fun toIndexPath(sequencialIndex: Int): IndexPath {
-            return IndexPath(0, 0)
-        }
+        private val listOfRowState: List<RowState>
+            get() {
+                val mutable = ArrayList<RowState>()
+                Section.values().forEach { section ->
+                    mutable.add(RowState.SectionRow(section))
+                    section.items.forEach { item ->
+                        mutable.add(RowState.ItemRow(item))
+                    }
+                }
+                return mutable
+            }
+
+        val numberOfSections: Int
+            get() = Section.values().size
     }
 
-    enum class Section(private val index: Int,
-                       private val title: String,
-                       private val items: List<Item>) {
+    enum class Section(val index: Int,
+                       val title: String,
+                       val items: List<Item>) {
         BASIC(0, "Basic Data Interaction", listOf(Item.BASIC_FETCH)),
         RECYCLER_VIEW(1, "Recycler View", emptyList()),
         USER_INPUT(2, "User Input", emptyList());
+
+        val numberOfItems: Int
+            get() = items.size
     }
 
     enum class Item(private val title: String, private val isAvailable: Boolean) {
