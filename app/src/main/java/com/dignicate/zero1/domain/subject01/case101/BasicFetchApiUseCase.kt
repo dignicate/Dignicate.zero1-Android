86b14ecf7a3e7101a1d50bd403794fd5f1288724
Xinyiqi <shinichi_watanabe@dignicate.com>
2021-05-29 12:34:37 +0900

    wip, flatMapSingle

--- app/src/main/java/com/dignicate/zero1/domain/subject01/case101/BasicFetchApiUseCase.kt
+++ app/src/main/java/com/dignicate/zero1/domain/subject01/case101/BasicFetchApiUseCase.kt
@@ -1,15 +1,28 @@
 package com.dignicate.zero1.domain.subject01.case101
 
+import com.dignicate.zero1.domain.subject01.CompanyInfo
 import com.dignicate.zero1.domain.subject01.SimpleCompanyInfoRepositoryInterface
 import com.dignicate.zero1.rx.DisposeBag
+import com.dignicate.zero1.rx.disposedBy
+import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
+import timber.log.Timber
 
 class BasicFetchApiUseCase(private val disposeBag: DisposeBag,
                            repository: SimpleCompanyInfoRepositoryInterface) {
 
-    private val fetchTrigger = PublishSubject.create<Int>()
+    private val fetchTrigger = PublishSubject.create<CompanyInfo.Id>()
 
     init {
+        fetchTrigger
+            .flatMapSingle {
+                repository.fetch(it)
+            }
+            // TODO: Implement bind() extension.
+            .subscribe()
+            .disposedBy(disposeBag)
+
 //        fetchTrigger
 //            .flatMap {
 //                repository.fetch(it)
@@ -19,6 +32,6 @@ class BasicFetchApiUseCase(private val disposeBag: DisposeBag,
     }
 
     fun fetch(id: Int) {
-        fetchTrigger.onNext(id)
+        fetchTrigger.onNext(CompanyInfo.Id(id))
     }
 }
