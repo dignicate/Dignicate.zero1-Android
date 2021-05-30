448d4ec191bfc3cd4a457652b9ee122822cb3eda
Xinyiqi <shinichi_watanabe@dignicate.com>
2021-05-30 10:28:29 +0900

    background

--- app/src/main/java/com/dignicate/zero1/infra/mock/subject01/SimpleCompanyInfoRepositoryMock.kt
+++ app/src/main/java/com/dignicate/zero1/infra/mock/subject01/SimpleCompanyInfoRepositoryMock.kt
@@ -3,16 +3,19 @@ package com.dignicate.zero1.infra.mock.subject01
 import com.dignicate.zero1.domain.subject01.CompanyInfo
 import com.dignicate.zero1.domain.subject01.SimpleCompanyInfoRepositoryInterface
 import io.reactivex.Single
+import kotlin.concurrent.thread
 
 class SimpleCompanyInfoRepositoryMock : SimpleCompanyInfoRepositoryInterface {
     override fun fetch(id: CompanyInfo.Id): Single<CompanyInfo> =
         Single.create { callback ->
-            Thread.sleep(2000L)
-            callback.onSuccess(
-                CompanyInfo(
-                    nameJP = "ディグニケート合同会社",
-                    nameEN = "Dignicate, LLC"
+            thread {
+                Thread.sleep(2000L)
+                callback.onSuccess(
+                    CompanyInfo(
+                        nameJP = "ディグニケート合同会社",
+                        nameEN = "Dignicate, LLC"
+                    )
                 )
-            )
+            }
         }
 }
