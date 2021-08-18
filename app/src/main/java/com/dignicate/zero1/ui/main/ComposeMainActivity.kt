434c6cf2a14b4e96a36dbf994dd05c7277eaee66
Xinyiqi <shinichi_watanabe@dignicate.com>
2021-08-18 09:26:17 +0900

    Success to list items.

--- app/src/main/java/com/dignicate/zero1/ui/main/ComposeMainActivity.kt
+++ app/src/main/java/com/dignicate/zero1/ui/main/ComposeMainActivity.kt
@@ -3,21 +3,43 @@ package com.dignicate.zero1.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.dignicate.zero1.ui.theme.Dignicatezero1Theme
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
 
 class ComposeMainActivity : ComponentActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         setContent {
-            Dignicatezero1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Column(Modifier.padding(1.dp)) {
+                Header("Test")
+                Item(1, "Item 1")
+            }
+        }
+    }
+}
+
+@Composable
+private fun Content(menus: List<MainViewModel.RowState>) {
+    Column(Modifier.padding(1.dp)) {
+        menus.forEach {
+            when (it) {
+                is MainViewModel.RowState.SectionRow -> {
+                    Header(it.section!!.title)
+                }
+                is MainViewModel.RowState.ItemRow -> {
+                    Item(it.indexedItem!!.second, it.indexedItem!!.first.title)
                 }
             }
         }
@@ -25,14 +47,20 @@ class ComposeMainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun Header(title: String) {
+    Text(text = title)
+}
+
+@Composable
+private fun Item(number: Int, title: String) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text("$number")
+        Text(title)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    Dignicatezero1Theme {
-        Greeting("Android")
-    }
+private fun Preview() {
+    Content(MainViewModel.ContentStructure.rowStates)
 }
